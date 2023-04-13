66b6948d2b6af4e30c6acf5c7607b1d68e3ed049
VictoriaSap <vicakyligina@gmail.com>
2023-04-13 16:32:33 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import calc from './modules/calc';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import {openModal} from './modules/modal';
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const modalTimeId = setTimeout(() => openModal('.modal',modalTimeId ), 50000)
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items','tabheader__item_active');
+    modal('[data-modal]','.modal', modalTimeId);
+    timer('.timer', '2023-02-12');
+    cards();
+    calc();
+    forms('form', modalTimeId);
+    slider({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner' 
+    });
+});
+
+
